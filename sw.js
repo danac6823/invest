32bb573bac258ac999f4b896839c93076cef5f80
danac6823 <danac6823@gmail.com>
2026-06-24 21:06:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'invest-v28';
+const CACHE = 'invest-v38';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
